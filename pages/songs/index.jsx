a6f9d016cd9eeab075e2e769dc8fa2af81f81868
Larissa Ramos <devlarissasr@gmail.com>
2022-07-03 10:41:04 -0300

    init

---
+++ pages/songs/index.jsx
@@ -0,0 +1,15 @@
+import styles from '../../styles/Songs.module.css';
+import Heart from '../../components/heart'
+
+export default function Songs(){
+    return(
+        <>
+            <Heart/>
+            <div>
+                <p className={styles.teste}>teste</p>
+            </div>
+        </>
+    )
+
+}
+
--- src/service/TestService.js
+++
@@ -1,7 +0,0 @@
-import api from "./api";
-
-export default class TestService{
-    static getFront( url ) {
-        return api.get( url );
-    }
-}
